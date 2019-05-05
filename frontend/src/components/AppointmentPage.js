36b9b037340d725806b18216c53e24541cd17a43
[KjHop] <[kjhop@wp.pl]>
2019-05-05 17:54:00 +0200

    Added AppointmentPage component and stylesheet

---
+++ frontend/src/components/AppointmentPage.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import "../styles/AppointmentPage.css"
+
+class AppointmentPage extends React.Component{
+    render(){
+        return(
+            <div className="AppointmentPage">
+                <h1>Make your appointment</h1>
+                <p>Crazy appointment interface comming here</p>
+            </div>
+        );
+    }
+}
+
+export default AppointmentPage;
--- frontend/src/components/Router.js
+++ frontend/src/components/Router.js
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import {Switch, Route} from 'react-router-dom';
 import Main from './Main';
+import AppointmentPage from './AppointmentPage';
 
 class Router extends React.Component{
     render(){
         return(
             <Switch>
+                <Route path='/appointment' component={AppointmentPage}/>
                 <Route path='/' component={Main}/>
             </Switch>
         );
