8a88f9c4d3c3ffd9f69b3db13cad435f61339c45
KjHop <kjhop@wp.pl>
2019-05-20 17:21:20 +0200

    Working on test api

--- backend/functions/routes.js
+++ backend/functions/routes.js
@@ -11,7 +11,18 @@ module.exports = app =>{
     app.post('/add-appointment', (req, res)=>{
         const db = new dataBase();
         let query = `INSERT INTO appointments(title, date, description) VALUES ('${req.body.title}', '${req.body.date}', '${req.body.description}')`;
-        db.connection.query(query);
+        var results;
+        db.connection.query('SELECT DATE_FORMAT(date, \'%Y-%m-%d\') AS date FROM appointments ', (err, results, fields)=>{
+            console.log(results[1].date);
+        });
+        db.endConnection();
+    });
+    app.post('/add', (req, res)=>{
+        const db = new dataBase();
+        let query = `INSERT INTO appointments(title, date, description) VALUES ('${req.body.title}', '${req.body.date}', '${req.body.description}')`;
+        db.connection.query(query, (err, results, fields)=>{
+            console.log(results);
+        });
         db.endConnection();
     });
 }
